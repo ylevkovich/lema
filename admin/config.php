91f9f8fcdb4fe7ba8912a65a711210a8fe100424
ylevkovich <yu.destroyer.ok@gmail.com>
2016-03-27 14:01:12 +0300

    first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://lema/admin/');
+define('HTTP_CATALOG', 'http://lema/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://lema/admin/');
+define('HTTPS_CATALOG', 'http://lema/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/lema/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/lema/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/lema/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/lema/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/lema/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/lema/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/lema/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/lema/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/lema/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/lema/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/lema/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/lema/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'lema');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://lema/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://lema/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/lema/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/lema/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/lema/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/lema/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/lema/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/lema/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/lema/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/lema/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/lema/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/lema/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/lema/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'lema');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
